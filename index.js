24b2239eceb0287b492c55417f7e05fbc99790f7
Blase1337 <114041435+Blase1337@users.noreply.github.com>
2024-05-10 15:07:48 -0400

    Added tracking of player score and human score, if there is a draw, round not counted

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var humanScore = 0;
+var computerScore = 0;
 let getComputerChoice = () => { // gets computer choices returns choice based on random number 1-3
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
@@ -17,31 +19,52 @@ let playRound = (playerSelection, computerSelection) => {
     console.log(computerSelection);
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
+        humanScore ++;
        return `Player won ${playerSelection} beats ${computerSelection}!`
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore ++;
         return `Computer won ${computerSelection} beats ${playerSelection}!`;
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
+        humanScore ++;
         return `Player won ${playerSelection} beats ${computerSelection}!`
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore ++;
         return `Computer won ${computerSelection} beats ${playerSelection}!`;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
+        humanScore ++;
         return `Player won ${playerSelection} beats ${computerSelection}!`;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore ++;
         return `Computer won ${computerSelection} beats ${playerSelection}!`;
     }
     else {
-        return `It was a draw!`;
+        let draw = "draw";
+        return {
+            message: "It was a draw!",
+            variable: draw
+        };
     }
 }
 
 let game = () => {
-    for (let i = 0; i<5; i++) {
+    var i=0
+    while ( i<5){
         console.log(playRound());
+        i++
+        if (playRound.varaible === "draw") {
+            i--
+        }
+    }
+    if (humanScore > computerScore) {
+        console.log(`Player won, Score: ${humanScore} : ${computerScore}`);
+    }
+    else {
+        console.log(`Computer won, Score: ${computerScore} : ${humanScore}`)
     }
 }
 game();
